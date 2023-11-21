651db03a9b77f4b022cc80cb54dedf898e94fa06
Johan Van <108252291+evgenycc@users.noreply.github.com>
2023-11-21 19:26:31 +0600

    Add files via upload

--- wh_parsers/date_parse.py
+++ wh_parsers/date_parse.py
@@ -9,13 +9,19 @@ months = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "Jun": "06", "Jul"
 
 
 def date_parse(date: str):
+    if ("(Changed)" in date and " AT " in date) or ("(Assigned)" in date and " AT "):
+        date = date.split()[-2]
+        return dtm.strptime(parse(date).strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S")
+    if " #" in date:
+        date = date.split()[0]
+        return dtm.strptime(parse(date).strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S")
     if "before " in date:
         dt = date.replace("before ", "")
         date = f'{dt.split("-")[1]}-{dt.split("-")[0].replace(dt.split("-")[0], months[dt.split("-")[0]])}-01 00:00:00'
         return dtm.strptime(parse(date).strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S")
     if "(GMT" in date or "(UTC" in date:
         return dtm.strptime(parse(date.split("(")[0].strip()).strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S")
-    if date.strip() == "-" or not date.strip():
+    if date.strip() == "-" or not date.strip() or "N/A" in date.strip():
         return None
     if " each year" in date:
         year = dtm.now().year + 1
